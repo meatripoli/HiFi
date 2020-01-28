32fc8aa47eb681b65ff326aacf16e2259a29d99b
Alex Stevens <zandra630@gmail.com>
2020-01-28 15:29:31 -0600

    Sets up the app to use passport for user authentication

---
+++ config/passport.js
@@ -0,0 +1,44 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const db = require("../models");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email"
+    },
+    function(email, password, done) {
+      db.User.findOne({
+        where: {
+          email: email
+        }
+      }).then(function(dbUser) {
+        if (!dbUser) {
+          return done(null, false, {
+            message: "Incorrect email."
+          });
+        } else if (!dbUser.validPassword(password)) {
+          return done(null, false, {
+            message: "Incorrect password."
+          });
+        }
+        return done(null, dbUser);
+      });
+    }
+  )
+);
+
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+// Just consider this part boilerplate needed to make it all work
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Exporting our configured passport
+module.exports = passport;
