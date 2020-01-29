cfb75859e7b0bddf093ba55de58ea868bff9205b
Maria Elena Tripoli <meatripoli@gmail.com>
2020-01-28 20:41:15 -0600

    added the routes shell for the album search and creating review

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,25 +2,27 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  app.get("/search/api/:album", function(req, res) {
+    let album = req.params.album;
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.json(dbExamples);
+    // });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Create a new review of album
+  app.post("/search/review", function(req, res) {
+    ///album id and user id will be pulled from the html divs
+    // db.Example.create(req.body).then(function(dbExample) {
+    //   res.json(dbExample);
+    // });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   // USER => Api routes
   app.get("/api/users", function(req, res) {
