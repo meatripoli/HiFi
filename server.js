6def00f525c9a4276832a73244deca01ae08ba79
Farai Matangira <fmatangira@gmail.com>
2020-01-29 22:32:32 -0600

    fixed server.js issue

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
