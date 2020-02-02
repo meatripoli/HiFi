b1ae1e96fbd9ae9a26355d7a252dddb83c1794bc
Alex Stevens <zandra630@gmail.com>
2020-02-02 13:24:52 -0600

    Adds employee records for our group to the seeder json

--- views/authenticatedLayout.js
+++ views/authenticatedLayout.js
@@ -46,7 +46,7 @@ const render = function(title, employee, body) {
   </div>
    <!-- END navbar-->
       ${body}
-   <!-- Scripts-->
+   <!-- Scripts Bootstrap-->
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
       integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
@@ -56,7 +56,8 @@ const render = function(title, employee, body) {
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
       integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
-    <!-- <script src="../public/js/app.js"></script> -->
+    <!-- Scripts js files-->
+    <script src="../public/js/app.js"></script> 
     <script src="../public/js/search.js"></script>
   </body>
   
