7ec57462e0e5d3f8bb17e2bdfaed5fdf51c851e0
Alex Stevens <zandra630@gmail.com>
2020-02-04 21:53:47 -0600

    Adds image to employee page:

--- views/employee.js
+++ views/employee.js
@@ -1,30 +1,27 @@
 const render = function(user) {
   return `
   <div class="container employee-view">
-  <div class="row top">
-    <div class="col">
+    <div class="row top">
+      <div class="col"></div>
     </div>
-  </div>
-  <div class="employee-table">
-    <div class="row align-items-center table-row name">
-      <div class="col col-sm-2 col-label name-label">
-        Name:
+  <div class="row">
+    <div class="col-md-5 img">
+      <img src="./img/headphones-sm.jpeg"  alt="" class="img-rounded">
+    </div>
+    <div class="col-md-7 employee-table">
+      <h2> ${user.employee.first_name} ${user.employee.last_name}</h2>  
+      <div class="row table-row email">
+        <div class="col col-label email-label">Email:</div>
+        <div class="col col-data email-employee">${user.email}</div>
       </div>
-      <div class="col col-data name-employee">
-        ${user.employee.first_name} ${user.employee.last_name}
+      <div class="row table-row store">
+        <div class="col col-label store-label">Store:</div>
+        <div class="col col-data store-employee">${user.employee.store.Name}</div>
       </div>
-    </div>
-    <div class="row align-items-center table-row email">
-      <div class="col col-sm-2 col-label email-label">Email:</div>
-      <div class="col col-data email-employee">${user.email}</div>
-    </div>
-    <div class="row align-items-center table-row store">
-      <div class="col col-sm-2 col-label store-label">Store:</div>
-      <div class="col col-data store-employee">${user.employee.store.Name}</div>
-    </div>
-    <div class="row align-items-center table-row fav-album">
-      <div class="col col-sm-2 col-label album-label">Favorite Album</div>
-      <div class="col col-data album-employee">${user.employee.fav_album}</div>
+      <div class="row table-row fav-album">
+        <div class="col col-label album-label">Favorite Album</div>
+        <div class="col col-data album-employee">${user.employee.fav_album}</div>
+      </div>
     </div>
   </div>
 </div>
