e16f9faf7474dd6ee6f055e80fcb57ebd9ee7d5a
Maria Elena Tripoli <meatripoli@gmail.com>
2020-02-04 18:12:49 -0600

    weird error for absolutely no reason

--- routes/album_api_route.js
+++ routes/album_api_route.js
@@ -4,7 +4,7 @@ const detail = require("../views/detail.js");
 const searchList = require("../views/searchResultList.js");
 
 module.exports = function(app) {
-  app.get("/api/album/:album", function(req, res) {
+  app.get("/api/albums/:album", function(req, res) {
     console.log("inside server get req")
     console.log(req.params.album);
     //below will be the code that searches the Album table for all albums called [req.body.searchInput]
@@ -17,15 +17,13 @@ module.exports = function(app) {
       console.log(album[0].dataValues);
       res.json(album);
       ///send json back client will tackle the rendering to page
-      //or I push the new page with the rendered info
-      //idk yet
     });
   });
 
     // req.body => first_name, last_name, address1, address2, 
-    // app.post("/api/albums", function(req, res) {
-    //   db.Album.create(req.body).then( album => {
-    //     res.json(album);
-    //   });
-    // });
+    app.post("/api/albums", function(req, res) {
+      db.Album.create(req.body).then( album => {
+        res.json(album);
+      });
+    });
 };
