b2a1a5ebb2a4dbe8a3305455e913b54ef0e3a5c2
Farai Matangira <fmatangira@gmail.com>
2020-02-04 21:15:39 -0600

    added the rest of the letters for the drop down

--- views/search-alex.js
+++ views/search-alex.js
@@ -50,6 +50,26 @@ var render = function (modalContent,searchContent) {
           <option>D</option>
           <option>E</option> 
           <option>F</option>
+          <option>G</option> 
+          <option>H</option> 
+          <option>I</option>
+          <option>J</option>
+          <option>K</option> 
+          <option>L</option>
+          <option>M</option> 
+          <option>N</option> 
+          <option>O</option>
+          <option>P</option>
+          <option>Q</option> 
+          <option>R</option>
+          <option>S</option> 
+          <option>T</option> 
+          <option>U</option>
+          <option>V</option>
+          <option>W</option> 
+          <option>X</option>
+          <option>Y</option> 
+          <option>Z</option>
         </select> 
       
         </nav>
