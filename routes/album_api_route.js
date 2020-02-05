1f252cd3996a8629a6bdc0d077b9bd7bf4ba2c4d
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-02-04 23:17:30 -0600

    Added route for looking up all albums, added axios package for last.fm albuma art lookup, and did 0-50 in album db artwork

--- routes/album_api_route.js
+++ routes/album_api_route.js
@@ -1,4 +1,5 @@
 const db = require('../models');
+const axios = require('axios');
 
 module.exports = function(app) {
   app.get("/api/albums/:album", function(req, res) {
@@ -32,4 +33,25 @@ module.exports = function(app) {
       console.log(album);
     });
   });
+
+  app.get("/api/album", function(req, res) {
+    console.log("inside server get req")
+    const output = [];
+
+    //below will be the code that searches the Album table for all albums called [req.body.searchInput]
+    db.album.findAll().then( album => {  
+      
+      for (var i = 0; i <= 50; i++) {
+        QueryURL2 = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${album[i].Album}&api_key=32ac8da476ac9c4a40ed6e41c042107d&format=json`;
+        
+        axios.get(QueryURL2)
+        .then(function(data){
+          var Art = data.data.results.albummatches.album[0].image[3];
+          output.push(Art);
+          console.log(output)
+           
+        });
+      }
+  });
+});
 };
