e00ac5a00bd65b654fa0c80ed9caeb3109caf089
Maria Elena Tripoli <meatripoli@gmail.com>
2020-02-05 18:06:06 -0600

    now its populating the stock tab and the album info

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,6 @@
 let listHTML = "";
+let modalTableHTML = "";
+let modalInfoHTML = "";
 // Set input focus when user navigates to pages
 $("#textBox").focus();
 
@@ -17,51 +19,28 @@ $(".searchButton").on("click", function(event) {
 
 $(document).on("click", ".search-link", function(event) {
   event.preventDefault();
-  console.log("pressing Album or Artist button");
   let resultID = $('.search-link').data('id');
   $("#detailModal").show();
   $.ajax({
     type: "GET",
     url: `/api/result/${resultID}`
   }).then(function(data) {
-      console.log(data);
+      createHTMLModal(data);
   });
 });
 
 // When the user clicks on <span> (x), close the modal
 $(document).on("click", ".close", function(event){
-  console.log("pressing X to close");
-  console.log(event.target)
   $("#detailModal").hide();
 });
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == document.getElementById("detailModal")) {
-    console.log("pressing anywhere outside modal");
     $("#detailModal").hide();
   };
 };
   
-function createHTMLList(obj){
-  console.log(obj);
-  obj.forEach(element => {
-    if (element.id === 0){
-      alert(`Album ${element.Album} was not found`);
-    }
-    else{
-      listHTML = `${listHTML}
-      <tr>
-      <td><a href="" class="search-link" data-id=${element.id}>${element.Album}</a></td>
-      <td>${element.Artist}</td>
-      <td>${element.Year}</td>
-      <td>${element.Genre}</td>
-      </tr>`;
-    }
-  }); 
-  $("#searchlist").html(listHTML);
-}
-
 // AJAX for pagination on search page
 $(".page-link").on("click",function(){
     var buttonValue = $(this).text()
@@ -82,3 +61,49 @@ $(".page-link").on("click",function(){
         }
     });
 });
+
+function createHTMLList(obj){
+  obj.forEach(element => {
+    if (element.id === 0){
+      alert(`Album ${element.Album} was not found`);
+    }
+    else{
+      listHTML = `${listHTML}
+      <tr>
+      <td><a href="" class="search-link" data-id=${element.id}>${element.Album}</a></td>
+      <td>${element.Artist}</td>
+      <td>${element.Year}</td>
+      <td>${element.Genre}</td>
+      </tr>`;
+    }
+  }); 
+  $("#searchlist").html(listHTML);
+}
+
+function createHTMLModal(objArray){
+  objArray.forEach(obj => {
+    //done to catch albums with no images
+    let img = obj.Img === null ? "../img/RTJ2.jpg" : obj.Img
+    modalTableHTML = `${modalTableHTML}
+      <tr>
+        <td>${obj.Name}</td>
+        <td>${obj.stock}</td>
+        <td>${obj.stock}</td>
+        <td>0</td>
+      </tr>`
+    modalInfoHTML = `
+      <div class="col-lg-6 albumCoverPic">
+        <img src="${img}" alt="Album art here" style="height: 250px; width:250px">
+      </div>
+      <div class="col-lg-6 albumInfo">
+        <ul class="albumInfoList">
+          <li>Album: ${obj.Album}</li>
+          <li>Artist: ${obj.Artist}</li>
+          <li>Year: ${obj.Year}</li>
+          <li>Genre: ${obj.Genre}</li>
+        </ul>
+      </div>`
+    $("#albuminfo").html(modalInfoHTML);
+    $("#albumstock").html(modalTableHTML)
+  });
+}
--- views/detail.js
+++ views/detail.js
@@ -6,18 +6,7 @@ var render = function () {
             <button type="submit" class="close">x</button>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-6 albumCoverPic">
-                <img src="../img/RTJ2.jpg" alt="Album art here" style="height: 250px; width:250px">
-            </div>
-            <div class="col-lg-6 albumInfo">
-                <ul class="albumInfoList">
-                    <li>Artist:</li>
-                    <li>Year:</li>
-                    <li>Condition:</li>
-                    <li>Price:</li>
-                </ul>
-            </div>
+        <div class="row" id="albuminfo">
         </div>
     </div>
     <!-- Classic tabs -->
@@ -34,61 +23,40 @@ var render = function () {
                             <a class="nav-link" id="review-tab-just" data-toggle="tab" href="#review" role="tab"
                                 aria-controls="review-just" aria-selected="false">Review</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="trackList-tab-just" data-toggle="tab" href="#trackList" role="tab"
-                                aria-controls="trackList-just" aria-selected="false">Track List</a>
-                        </li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
     <div class="tab-content card" id="myTabContentJust">
-        <div class="tab-pane fade show active" id="stock" role="tabpanel" aria-labelledby="stock-tab-just">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Location</th>
-                        <th>Stock</th>
-                        <th>New</th>
-                        <th>Used</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Location A</td>
-                        <td>7</td>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
-                    <tr>
-                        <td>Location B</td>
-                        <td>3</td>
-                        <td>1</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>Location C</td>
-                        <td>5</td>
-                        <td>2</td>
-                        <td>3</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="tab-pane fade" id="review" role="tabpanel" aria-labelledby="Reviews-tab-classic">
-            <p>This album rocks! Literally got me through the summer of '89! Track 8 is a personal favorite, and track 3 is
-                one for the ages!</p>
-            <p>Reviewed by: Melon</p>
-            <!---Talk about adding review posting here instead of in the earlier page as that makes better sense-->
-        </div>
-        <div class="tab-pane fade" id="trackList" role="tabpanel" aria-labelledby="TrackList-tab-classic">
-            <ul style="list-style-type:decimal">
-                <li>Intro</li>
-                <li>Second Track</li>
-                <li>Album over</li>
-            </ul>
-        </div>
+    <div class="tab-pane fade show active" id="stock" role="tabpanel" aria-labelledby="stock-tab-just">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Location</th>
+          <th>Stock</th>
+          <th>New</th>
+          <th>Used</th>
+        </tr>
+      </thead>
+      <tbody id="albumstock">
+        
+      </tbody>
+    </table>
+  </div>
+  <div class="tab-pane fade" id="review" role="tabpanel" aria-labelledby="Reviews-tab-classic">
+    <p>This album rocks! Literally got me through the summer of '89! Track 8 is a personal favorite, and track 3 is
+      one for the ages!</p>
+    <p>Reviewed by: Melon</p>
+    <!---Talk about adding review posting here instead of in the earlier page as that makes better sense-->
+  </div>
+  <div class="tab-pane fade" id="trackList" role="tabpanel" aria-labelledby="TrackList-tab-classic">
+    <ul style="list-style-type:decimal">
+      <li>Intro</li>
+      <li>Second Track</li>
+      <li>Album over</li>
+    </ul>
+  </div>
     </div>`
 }
 
