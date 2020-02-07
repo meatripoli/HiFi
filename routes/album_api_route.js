2813e07c1c09224be17ac9303f46c3ba703e6dac
Maria Elena Tripoli <meatripoli@gmail.com>
2020-02-06 18:43:54 -0600

    added functionality to the search. It searches for all albums containing specific word entered

--- routes/album_api_route.js
+++ routes/album_api_route.js
@@ -5,7 +5,10 @@ module.exports = function(app) {
     //below will be the code that searches the Album table for all albums called [req.body.searchInput]
     db.album.findAll({
       where: {
-        album: req.params.album
+        //album: req.params.album
+        album: {
+          [db.Sequelize.Op.like]: `%${req.params.album}%`
+        }
       }
     }).then( album => {
       if(album.length === 0){
@@ -23,7 +26,7 @@ module.exports = function(app) {
       `SELECT b.stock,a.Year,a.Album,a.Artist,a.Genre,a.Img,a.Subgenre,c.Name,d.title,d.body
       FROM hifi_db.stocks b LEFT JOIN hifi_db.albums a ON a.id=b.albumId LEFT JOIN hifi_db.stores c ON b.storeId = c.id
       LEFT JOIN hifi_db.reviews d ON d.albumId=b.albumId where b.albumId=${req.params.id}`, 
-      { type: db.sequelize.QueryTypes.SELECT}
+      {type: db.sequelize.QueryTypes.SELECT}
     )
     .then(function(album) {
       console.log(album)
